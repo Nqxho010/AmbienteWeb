ae7f24df54cbd1d690c6d9c0682276747e673f38
Adrian Alvarado <adaz1989@gmail.com>
2024-11-17 12:37:10 -0600

    commit 1

---
+++ Adrian/vistas/layout/footer.php
@@ -0,0 +1,9 @@
+
+<script src="js/menuUsuario.js"></script>
+    <footer class="footer">
+        <div>
+            <p class="footer__derechos">&copy; 2024 Feria Virtual CR. Todos los derechos reservados.</p>
+        </div>
+    </footer>
+</body>
+</html>
