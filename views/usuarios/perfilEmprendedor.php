b41f4ccaeecbee189fefcbc0530fc734533efc51
Adrian Alvarado <adaz1989@gmail.com>
2024-11-17 19:41:23 -0600

    ultimo commit del dia
    dejo todas mis vistas en php dentro de la estructura principal.

---
+++ views/usuarios/perfilEmprendedor.php
@@ -0,0 +1,71 @@
+<?php
+$titulo = 'Perfil de Emprendedor';
+require_once '../layout/head.php';
+require_once '../layout/header.php';
+?>
+
+<div class="info-emprendedor">
+    <img class="info-emprendedor__img" src="https://answers-afd.microsoft.com/static/images/image-not-found.jpg">
+    <div class="info-emprendedor__cont-texto">
+        <h2 class="info-emprendedor__nombre">[Nombre emprendimiento]</h2>
+        <p class="info-emprendedor__detalle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+    </div>
+</div>
+
+<div class="cont-productos">
+    <h3 class="cont-productos__titulo">Productos</h3>
+    <div class="cont-productos__grupo">
+
+        <div class="producto">
+            <img class="producto__img" src="https://answers-afd.microsoft.com/static/images/image-not-found.jpg">
+            <h4 class="producto__nombre">[Nombre producto]</h4>
+            <p class="producto__detalle">[Detalle corto de producto para los preview]</p>
+            <p class="producto__precio">₡2500</p>
+            <button class="boton-verde">Agregar al carrito</button>
+        </div>
+
+        <div class="producto">
+            <img class="producto__img" src="https://answers-afd.microsoft.com/static/images/image-not-found.jpg">
+            <h4 class="producto__nombre">[Nombre producto]</h4>
+            <p class="producto__detalle">[Detalle corto de producto para los preview]</p>
+            <p class="producto__precio">₡2500</p>
+            <button class="boton-verde">Agregar al carrito</button>
+        </div>
+
+        <div class="producto">
+            <img class="producto__img" src="https://answers-afd.microsoft.com/static/images/image-not-found.jpg">
+            <h4 class="producto__nombre">[Nombre producto]</h4>
+            <p class="producto__detalle">[Detalle corto de producto para los preview]</p>
+            <p class="producto__precio">₡2500</p>
+            <button class="boton-verde">Agregar al carrito</button>
+        </div>
+
+        <div class="producto">
+            <img class="producto__img" src="https://answers-afd.microsoft.com/static/images/image-not-found.jpg">
+            <h4 class="producto__nombre">[Nombre producto]</h4>
+            <p class="producto__detalle">[Detalle corto de producto para los preview]</p>
+            <p class="producto__precio">₡2500</p>
+            <button class="boton-verde">Agregar al carrito</button>
+        </div>
+
+        <div class="producto">
+            <img class="producto__img" src="https://answers-afd.microsoft.com/static/images/image-not-found.jpg">
+            <h4 class="producto__nombre">[Nombre producto]</h4>
+            <p class="producto__detalle">[Detalle corto de producto para los preview]</p>
+            <p class="producto__precio">₡2500</p>
+            <button class="boton-verde">Agregar al carrito</button>
+        </div>
+
+
+    </div>
+
+
+</div>
+
+<?php
+require_once '../layout/footer.php';
+?>
