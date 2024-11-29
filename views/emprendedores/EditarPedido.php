b7dd477c9fa53e9b5c6b636e764ad585bd47bdf0
MinorMG <minorjose2015@gmail.com>
2024-11-28 22:40:10 -0600

    Vistas Corregidas pasadas a php
    Hace falta los botones pra que redirijan mañana lo hago

--- views/emprendedores/EditarPedido.php
+++ views/emprendedores/EditarPedido.php
@@ -7,22 +7,24 @@ require_once '../layout/header.php';
 
     <link rel="stylesheet" type="text/css" href="/AmbienteWeb/public/css/editarPedidos.css">
 
-    <h3 class="productosTitulo"> Mis productos > Editar</h3>
+    <h3 class="productosTitulo"> Mis Pedidos > Editar</h3>
 
     <div class="flex-container">
         <form class="form">
             
-            
-            <label for="nombre">Nombre del Producto:</label>
-            <input type="text" id="nombre" placeholder="Ingresar nombre del Producto:" required>
+            <label for="nombre">Nombre del Pedido:</label>
+            <input type="text" id="nombre_pedido" placeholder="Pedido de cremas" required>
             <br>
-            <label for="descripcion">Descripcion:</label>
-            <input type="number" id="salarioBruto" placeholder="Ingresa tu salario bruto" required>
+            <label for="Usuario">Usuario:</label>
+            <input type="text" id="nombre_usuario" placeholder="Juan Pereira" required>
             <br>
-            <label for="precio">Precio (₡):</label>
-            <input type="number" id="precio" placeholder="Ingresar Precio" required>
+            <label for="Usuario">Estado del pedido:</label>
+            <input type="text" id="estado_pedido" placeholder="Ingresa tu salario bruto" required>
             <br>
-            <label for="stock">Stock:</label>
+            <label for="fecha">Fecha del pedido:</label>
+            <input type="date" id="fecha" placeholder="11/28/2024" required>
+            <br>
+            <label for="stock">Cantidadd del pedido:</label>
             <input type="number" id="stock" placeholder="Ingresar Cantidad (Stock)" required>
             <br>
             <label for="img">Imagen:</label>
@@ -31,6 +33,7 @@ require_once '../layout/header.php';
             <button class="boton-verde" type="submit">Editar</button>
             <br>
             <button class="boton-rojo" type="submit">Eliminar</button>
+
         </form>
     </div>
     
---
+++ views/emprendedores/IndexPerfil.php
@@ -0,0 +1,44 @@
+<?php
+$titulo = 'Index Perfil';
+require_once '../layout/head.php';
+require_once '../layout/header.php';
+require_once '../layout/footer.php';
+?>
+    <link rel="stylesheet" type="text/css" href="/AmbienteWeb/public/css/Emprendedores.css">
+
+        <br>
+
+        <div class="flex-container">
+            <form class="form">
+
+            <label for="FotoPerfil">Foto de perfil:</label>
+            <input type="image" id="fotoperfil" placeholder="Juan" required>
+            <br>
+
+            <label for="nombre">Nombre del Usuario:</label>
+            <input type="text" id="nombre" placeholder="Juan" required>
+            <br>
+            <label for="nombre">Apellidos del Usuario:</label>
+            <input type="text" id="Emprendimiento" placeholder="Selecciona nombre del Producto:" required>
+            <br>
+            <label for="Categoria">Correo:</label>
+            <input type="email" id="idcategoria" placeholder="Seleccionar Categoria del Producto:" required>
+            <br>
+            <label for="descripcion">Provincia:</label>
+            <input type="text" id="salarioBruto" placeholder="Ingresa tu salario bruto" required>
+            <br>
+            <label for="precio">Direccion:</label>
+            <input type="text" id="precio" placeholder="Ingresar Precio" required>
+            <br>
+            <button class="boton-verde" type="submit">Editar</button>
+            <br>
+            <button class="boton-rojo" type="submit">Eliminar Perfil</button>
+            <button class="boton-rojo" type="submit">Cancelar</button>
+            </form>
+        </div>
+
+        <br>
+
+   
+    
+    <script src="js/menuUsuario.js"></script>
