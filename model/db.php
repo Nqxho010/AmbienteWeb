e4e7bcfd17d5e6a3dd7bde214b3b6b6e780cca05
MinorMG <minorjose2015@gmail.com>
2024-12-10 21:09:11 -0600

    carrousel index emp

--- model/db.php
+++ model/db.php
@@ -1,14 +1,12 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
 $database = "db_feria_virtual_cr";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
 if($conn->connect_error){
     die("Conexio fallida");
-}else{
-    echo"coneccion exitosa";
 }
 ?>
